ab23e4ee064d447f81c34a0c775c7d20da9e5b6d
Anuj Katra <anujkatra94@gmail.com>
2025-06-05 22:32:52 +0530

    Inital commit sanity schemas, plugins and helpers

---
+++ apps/studio/schemaTypes/singletons/index.ts
@@ -0,0 +1,10 @@
+/** Singletons */
+
+import {aboutUs} from './about-us-page'
+import {artists} from './artists'
+import {auctions} from './auctions'
+import {gallery} from './gallery'
+import {homepage} from './homepage'
+import {shows} from './shows'
+
+export const singletons = [homepage, aboutUs, gallery, artists, shows, auctions]
