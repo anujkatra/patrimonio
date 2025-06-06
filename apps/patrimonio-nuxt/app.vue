0ff1598cea6a75a489f2b342579c4091b87b7481
Yash Bajaj <=>
2025-06-06 18:25:51 +0530

    added sanity packages, added script to generate sanity types when app runs, brought sanity data on homepage

--- apps/patrimonio-nuxt/app.vue
+++ apps/patrimonio-nuxt/app.vue
@@ -1,6 +1,16 @@
+<script lang="ts" setup>
+import { homepageQuery } from "~/sanity/queries";
+import type { HomepageQueryResult } from "~/sanity/types";
+
+const { data } = await useSanityQuery<HomepageQueryResult>(homepageQuery);
+
+// useSiteMetadata({
+//   title: post?.seo?.title || post?.value?.title,
+//   description: post?.value?.seoDescription || post?.value?.excerpt,
+// });
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+ <p>test</p>
 </template>
+
