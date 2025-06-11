d9aa5d152f68a3fd98a0c5c10393ea5c076e9c51
Yash Bajaj <=>
2025-06-11 18:15:17 +0530

    added logo to header

--- apps/patrimonio-nuxt/components/Header.vue
+++ apps/patrimonio-nuxt/components/Header.vue
@@ -3,7 +3,10 @@
 
 <template>
   <header
-    class="fixed z-50 h-24 inset-0 bg-white/80 flex backdrop-blur-lg"
+    class="fixed z-50 h-24 inset-0 bg-white/80 flex justify-between backdrop-blur-lg p-6"
   >
+  <NuxtLink class="flex items-center gap-2" to="/">
+         <NuxtImg src="/logo.jpg"/>
+        </NuxtLink>
      </header>
 </template>
