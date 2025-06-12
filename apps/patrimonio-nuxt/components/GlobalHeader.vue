e9560cab1999a4c6d20350ef2310574d79b72194
Yash Bajaj <=>
2025-06-12 20:31:57 +0530

    added prettier and formatted all files

--- apps/patrimonio-nuxt/components/GlobalHeader.vue
+++ apps/patrimonio-nuxt/components/GlobalHeader.vue
@@ -1,28 +1,25 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <header
-    class="fixed z-50 h-24 inset-0 bg-white/80 flex justify-between backdrop-blur-lg p-6"
-  >
-  <NuxtLink class="flex items-center gap-2" to="/">
-         <NuxtImg src="/logo.jpg"/>
-        </NuxtLink>
-         <nav class="flex align-middle">
-          <ul
-            role="list"
-            class="flex items-center gap-6 md:gap-8 leading-5 text-sm md:text-base tracking-tight font-normal"
-          >
-            <li>
-              <NuxtLink to="/artists" class="hover:underline">Artists</NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/gallery" class="hover:underline">Gallery</NuxtLink>
-            </li>
-            <li>
-              <NuxtLink to="/about-us" class="hover:underline">About Us</NuxtLink>
-            </li>
-          </ul>
-        </nav>
-     </header>
+  <header class="fixed z-50 h-24 inset-0 bg-white/80 flex justify-between backdrop-blur-lg p-6">
+    <NuxtLink class="flex items-center gap-2" to="/">
+      <NuxtImg src="/logo.jpg" />
+    </NuxtLink>
+    <nav class="flex align-middle">
+      <ul
+        role="list"
+        class="flex items-center gap-6 md:gap-8 leading-5 text-sm md:text-base tracking-tight font-normal"
+      >
+        <li>
+          <NuxtLink to="/artists" class="hover:underline">Artists</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/gallery" class="hover:underline">Gallery</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/about-us" class="hover:underline">About Us</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
