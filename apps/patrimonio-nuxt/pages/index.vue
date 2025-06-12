2bee517adcbed83d1cb6669e4aeee9deae1dfd14
Anuj Katra <anujkatra94@gmail.com>
2025-06-12 19:13:38 +0530

    removed test message from hp

--- apps/patrimonio-nuxt/pages/index.vue
+++ apps/patrimonio-nuxt/pages/index.vue
@@ -12,7 +12,6 @@ useSiteMetadata({
 </script>
 
 <template>
- <p class="text-center">index vue test</p>
  <p class="text-center">{{homepageData?.title}}</p>
  <p class="text-center">{{homepageData?.landingText}}</p>
  <NuxtImg provider="sanity" :src="`${homepageData?.landingImage.asset?._id}`" :alt="`${homepageData?.landingImage.alt}`" sizes="md:400px" class="mx-auto" />
