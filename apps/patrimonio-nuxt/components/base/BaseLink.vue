f8083d8f907f0d04157eb1362c690615bdf4b9ef
Anuj Katra <anujkatra94@gmail.com>
2025-07-01 17:02:22 +0530

    added functionality to allow overriding link styling

--- apps/patrimonio-nuxt/components/base/BaseLink.vue
+++ apps/patrimonio-nuxt/components/base/BaseLink.vue
@@ -6,6 +6,7 @@ import {cva, type VariantProps} from 'class-variance-authority'
 const props = defineProps<{
   variant?: VariantProps<typeof inputVariants>['variant']
   icon?: boolean
+  class?: string
 }>()
 
 const inputVariants = cva(
@@ -37,6 +38,7 @@ const linkClasses = computed(() =>
       variant: props.variant,
       icon: props.icon,
     }),
+    props.class,
   ),
 )
 </script>
