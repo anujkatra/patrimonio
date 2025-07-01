16f9d132b95c8fabfaad5e2a4a38cd2067be2b4a
Yash Bajaj <=>
2025-07-01 16:37:41 +0530

    homepage cleanup

--- apps/patrimonio-nuxt/pages/index.vue
+++ apps/patrimonio-nuxt/pages/index.vue
@@ -2,10 +2,8 @@
 import {homepageQuery} from '~/sanity/queries'
 import type {HomepageQueryResult} from '~/sanity/types'
 import Arrow from '~/assets/svg/arrow.svg'
-import {cn} from '@/utils/cn'
 
 const {data: homepageData} = await useSanityQuery<HomepageQueryResult>(homepageQuery)
-const isActive = true
 
 useSiteMetadata({
   title: homepageData?.value?.seo?.title ?? 'title',
@@ -18,10 +16,6 @@ useSiteMetadata({
   <div class="flex flex-col gap-8 justify-center">
     <p class="text-center">{{ homepageData?.title }}</p>
     <p class="text-center">{{ homepageData?.landingText }}</p>
-    <Arrow
-      :class="cn('size-10 text-amber-300', isActive && 'bg-blue-600 mx-auto')"
-      :font-controlled="false"
-    />
     <NuxtImg
       provider="sanity"
       :src="`${homepageData?.landingImage.asset?._id}`"
