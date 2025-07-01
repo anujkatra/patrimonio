0370cca0078c23e1b654e7c2b4b57267132e5b51
Yash Bajaj <yashbajaj234@gmail.com>
2025-07-01 16:36:56 +0530

    added link component variants

--- apps/patrimonio-nuxt/components/base/BaseLink.vue
+++ apps/patrimonio-nuxt/components/base/BaseLink.vue
@@ -1,32 +1,49 @@
 <script setup lang="ts">
 import {cn} from '@/utils/cn'
 import {computed} from 'vue'
-import Arrow from '~/assets/svg/arrow.svg'
+import {cva, type VariantProps} from 'class-variance-authority'
 
 const props = defineProps<{
-  variant: 'primary'
-  hasIcon: boolean
+  variant?: VariantProps<typeof inputVariants>['variant']
+  icon?: boolean
 }>()
 
-const variantClasses: Record<string, string> = {
-  primary: 'w-[420px] h-[60px] px-2.5 px-[30px] border-[1.5px]',
-}
+const inputVariants = cva(
+  'inline-flex transition-all items-center duration-300 ease-out font-cabinet text-[20px] w-[420px] h-[60px] px-2.5 px-[30px] border-[0.5px] border-[#202020]',
+  {
+    variants: {
+      variant: {
+        primary: 'bg-white hover:bg-black text-black hover:text-white border-[1.5px]',
+        secondary:
+          'bg-[linear-gradient(264.83deg,rgba(252,251,247,0.5)_-4.61%,rgba(129,178,219,0.5)_44.28%,rgba(214,51,46,0.5)_112.37%)] hover:bg-black hover:bg-none hover:text-white',
+        ghost: 'bg-transparent text-blue-600 hover:bg-blue-100',
+      },
+      icon: {
+        false: 'justify-center',
+        true: 'justify-between pr-[40px] hover:pr-[30px]',
+      },
+    },
+    defaultVariants: {
+      variant: 'primary',
+      icon: false,
+    },
+  },
+)
 
 const linkClasses = computed(() =>
   cn(
-    'inline-flex transition-all items-center duration-300 ease-in-out font-cabinet text-[20px]',
-    variantClasses[props.variant],
-    props.hasIcon
-      ? 'justify-between pr-[40px] hover:pr-[30px]'
-      : 'justify-center hover:text-white hover:bg-black',
+    inputVariants({
+      variant: props.variant,
+      icon: props.icon,
+    }),
   ),
 )
 </script>
 <template>
   <NuxtLink :class="linkClasses">
     <slot>Explore</slot>
-    <template v-if="props.hasIcon">
-      <Arrow class="w-[50px] text-black" :font-controlled="false"
-    /></template>
+    <span v-if="icon">
+      <slot name="icon" />
+    </span>
   </NuxtLink>
 </template>
