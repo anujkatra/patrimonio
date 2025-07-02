d888a4f7952f9a7b3a1731769fc5714c457bf3ca
Yash Bajaj <=>
2025-07-02 15:51:54 +0530

    added new footer component

---
+++ apps/patrimonio-nuxt/components/base/BaseFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer
+    class="bg-patrimonio-beige border-[#202020] w-full border-t-[0.5px] flex flex-col gap-[50px] pt-[50px] px-5 pb-[30px]"
+  >
+    <div class="flex flex-col gap-[50px] justify-center">
+      <NuxtLink class="w-fit" to="/">
+        <NuxtImg src="/logo.jpg" class="w-full" />
+      </NuxtLink>
+    </div>
+  </footer>
+</template>
