73dafa6f86cfdeab20d6696283913369cdbf4bbc
Yash Bajaj <yashbajaj234@gmail.com>
2025-07-07 22:52:05 +0530

    update css xl and 2xl breakpoint and add large viewport footer styles

--- apps/patrimonio-nuxt/components/base/BaseFooter.vue
+++ apps/patrimonio-nuxt/components/base/BaseFooter.vue
@@ -1,9 +1,9 @@
 <template>
   <footer
-    class="bg-patrimonio-beige border-[#202020] w-full border-t-[0.5px] flex flex-col gap-[50px] pt-[50px] px-5 pb-[30px] sm:pt-[70px] sm:px-10 sm:gap-[65px]"
+    class="bg-patrimonio-beige border-[#202020] w-full border-t-[0.5px] flex flex-col gap-[50px] pt-[50px] px-5 pb-[30px] sm:pt-[70px] sm:px-10 sm:gap-[65px] 2xl:px-[70px]"
   >
     <div class="flex flex-col gap-[50px] sm:flex-row sm:justify-between">
-      <NuxtLink to="/" class="w-full sm:max-w-[341px]">
+      <NuxtLink to="/" class="w-full sm:max-w-[341px] 2xl:max-w-[523px]">
         <NuxtImg src="/logo.jpg" alt="Website Logo" class="w-full" />
       </NuxtLink>
       <div class="flex flex-col gap-[30px] lg:flex-row lg:gap-[133px]">
