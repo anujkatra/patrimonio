e977c98ab1e415672537eb58ff1570420660399b
Yash Bajaj <=>
2025-07-08 23:03:35 +0530

    added header menu svgs and desktop styling

--- apps/patrimonio-nuxt/components/base/BaseHeader.vue
+++ apps/patrimonio-nuxt/components/base/BaseHeader.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import {ref, watch, onBeforeUnmount} from 'vue'
+import Cancel from '~/assets/svg/cancel.svg'
+import Menu from '~/assets/svg/menu.svg'
 
 type HeaderContentProps = {
   link: string
@@ -60,12 +62,16 @@ onBeforeUnmount(() => {
 
 <template>
   <header class="sticky z-50 top-0 bg-white/80">
-    <nav
-      :class="`w-full relative backdrop-blur-lg z-50 bg-white/80 ${isMobileMenuOpen ? `h-screen` : ``}`"
-    >
-      <div class="flex justify-between px-5 pb-5 pt-[50px] border-[#202020] border-b-[0.5px]">
+    <nav :class="`w-full relative z-50 bg-white/80 ${isMobileMenuOpen ? `h-screen` : ``}`">
+      <div
+        class="flex justify-between px-5 pb-5 pt-[50px] border-[#202020] border-b-[0.5px] xl:px-[70px] xl:h-[70px] xl:py-5 xl:gap-10"
+      >
         <NuxtLink class="flex items-center gap-2" to="/">
-          <NuxtImg src="/logo.jpg" class="w-full max-w-[170px] h-8" @click="closeMenu" />
+          <NuxtImg
+            src="/logo.jpg"
+            class="w-full max-w-[170px] md:max-w-[191px] md:h-[35px] h-8"
+            @click="closeMenu"
+          />
         </NuxtLink>
         <ul
           role="list"
@@ -74,44 +80,32 @@ onBeforeUnmount(() => {
           <li v-for="(header, index) in headerContent" :key="index">
             <NuxtLink :to="header.href" class="hover:underline">{{ header.link }}</NuxtLink>
           </li>
+          <li>
+            <NuxtImg src="/whatsapp.png" class="w-full h-[35px]" />
+          </li>
         </ul>
         <!-- Mobile Toggle -->
-        <button
-          class="lg:hidden focus:outline-none hover:cursor-pointer"
-          aria-label="Toggle menu"
-          @click="isMobileMenuOpen = !isMobileMenuOpen"
-        >
-          <svg
-            v-if="!isMobileMenuOpen"
-            class="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            viewBox="0 0 24 24"
+        <div class="lg:hidden flex gap-[15px]">
+          <NuxtImg src="/whatsapp.png" class="w-full h-[35px]" />
+          <button
+            class="focus:outline-none hover:cursor-pointer"
+            aria-label="Toggle menu"
+            @click="isMobileMenuOpen = !isMobileMenuOpen"
           >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
-          <svg
-            v-else
-            class="w-6 h-6"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            viewBox="0 0 24 24"
-          >
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
+            <Menu v-if="!isMobileMenuOpen" class="size-6" :font-controlled="false" />
+            <Cancel v-else class="size-6 text-white" :font-controlled="false" />
+          </button>
+        </div>
       </div>
       <Transition
         enter-from-class="opacity-0"
-        enter-active-class="transition duration-300 ease-in-out"
+        enter-active-class="transition duration-150 ease-in-out"
         leave-to-class="opacity-0"
-        leave-active-class="transition duration-300 ease-in-out"
+        leave-active-class="transition duration-150 ease-in-out"
       >
         <ul
           v-if="isMobileMenuOpen"
-          class="lg:hidden overflow-y-auto bg-white text-center w-full px-4"
+          class="lg:hidden absolute overflow-y-auto bg-white text-center w-full px-4"
         >
           <li
             v-for="(header, index) in headerContent"
