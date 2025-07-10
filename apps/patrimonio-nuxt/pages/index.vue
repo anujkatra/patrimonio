f6b894beca21dcebf59c30e7ad1b944519cefbf8
Yash Bajaj <yashbajaj234@gmail.com>
2025-07-11 00:19:49 +0530

    add mobile landing section styling and max width to baselink component

--- apps/patrimonio-nuxt/pages/index.vue
+++ apps/patrimonio-nuxt/pages/index.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import {homepageQuery} from '~/sanity/queries'
 import type {HomepageQueryResult} from '~/sanity/types'
+import Arrow from '~/assets/svg/arrow.svg'
 
 const {data: homepageData} = await useSanityQuery<HomepageQueryResult>(homepageQuery)
 
@@ -12,18 +13,29 @@ useSiteMetadata({
 </script>
 
 <template>
-  <main class="px-5 py-10">
-    <section>
-      <div class="flex flex-col justify-center gap-8">
-        <p class="text-center">{{ homepageData?.title }}</p>
-        <p class="text-center">{{ homepageData?.landingText }}</p>
+  <main class="px-5">
+    <section class="py-[50px]">
+      <div class="flex flex-col gap-5">
         <NuxtImg
           provider="sanity"
           :src="`${homepageData?.landingImage.asset?._id}`"
           :alt="`${homepageData?.landingImage.alt}`"
-          sizes="md:400px"
+          sizes="100vw sm:400px"
           class="mx-auto"
         />
+        <div class="flex flex-col gap-2.5">
+          <p class="font-cabinet text-[35px] leading-none font-normal tracking-normal">
+            {{ homepageData?.title }}
+          </p>
+          <div class="flex flex-col gap-5">
+            <p class="font-satoshi text-lg leading-none font-light tracking-normal">
+              {{ homepageData?.landingText }}
+            </p>
+            <BaseLink variant="secondary" icon to="/gallery">
+              <template #icon> <Arrow class="w-[50px]" :font-controlled="false" /></template>
+            </BaseLink>
+          </div>
+        </div>
       </div>
     </section>
   </main>
