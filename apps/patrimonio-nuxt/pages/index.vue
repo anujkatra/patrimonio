3598c0052cfa4e088c51ca664f25dfa15227f0cd
Yash Bajaj <=>
2025-07-11 00:33:09 +0530

    added tablet styles wip

--- apps/patrimonio-nuxt/pages/index.vue
+++ apps/patrimonio-nuxt/pages/index.vue
@@ -13,8 +13,8 @@ useSiteMetadata({
 </script>
 
 <template>
-  <main class="px-5">
-    <section class="py-[50px]">
+  <main>
+    <section class="px-5 py-[50px] md:px-10">
       <div class="flex flex-col gap-5">
         <NuxtImg
           provider="sanity"
@@ -23,15 +23,19 @@ useSiteMetadata({
           sizes="100vw sm:400px"
           class="mx-auto"
         />
-        <div class="flex flex-col gap-2.5">
-          <p class="font-cabinet text-[35px] leading-none font-normal tracking-normal">
+        <div
+          class="flex flex-col gap-2.5 self-center md:items-center md:max-lg:max-w-lg lg:flex-row lg:gap-0"
+        >
+          <p
+            class="font-cabinet text-[35px] leading-none font-normal tracking-normal lg:flex-1 lg:text-[50px]"
+          >
             {{ homepageData?.title }}
           </p>
-          <div class="flex flex-col gap-5">
+          <div class="flex flex-col gap-5 lg:flex-1">
             <p class="font-satoshi text-lg leading-none font-light tracking-normal">
               {{ homepageData?.landingText }}
             </p>
-            <BaseLink variant="secondary" icon to="/gallery">
+            <BaseLink variant="secondary" class="w-full" icon to="/gallery">
               <template #icon> <Arrow class="w-[50px]" :font-controlled="false" /></template>
             </BaseLink>
           </div>
