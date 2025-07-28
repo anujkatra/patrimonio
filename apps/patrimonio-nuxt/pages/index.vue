5e1dd811d7c3795f0ed5ab2cd89dec55d51bddfa
Anuj Katra <anujkatra94@gmail.com>
2025-07-28 16:36:16 +0530

    added press and media section

--- apps/patrimonio-nuxt/pages/index.vue
+++ apps/patrimonio-nuxt/pages/index.vue
@@ -314,6 +314,22 @@ function next(index: number) {
         </div>
       </div>
     </section>
+
+    <section class="flex justify-center border-t-[0.5px] border-[#202020] px-5 py-[50px] md:px-10">
+      <div class="flex w-full max-w-[1440px] flex-col gap-[50px]">
+        <div class="flex flex-col gap-2.5">
+          <h2
+            class="font-cabinet text-[32px] leading-none font-normal tracking-normal lg:flex-1 lg:text-[50px]"
+          >
+            Media & Press
+          </h2>
+          <p class="font-satoshi text-lg leading-none font-light tracking-normal">
+            Discover an exquisite global collection, thoughtfully presented through curated events,
+            exhibitions, and exclusive showcases.
+          </p>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
