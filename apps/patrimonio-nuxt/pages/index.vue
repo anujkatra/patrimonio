6228ab741520d3418d7315993c94afd2fe7ef476
Anuj Katra <anujkatra94@gmail.com>
2025-08-12 22:56:56 +0530

    removed vue grid component

--- apps/patrimonio-nuxt/pages/index.vue
+++ apps/patrimonio-nuxt/pages/index.vue
@@ -2,33 +2,9 @@
 import {homepageQuery} from '~/sanity/queries'
 import type {HomepageQueryResult} from '~/sanity/types'
 import Arrow from '~/assets/svg/arrow.svg'
-import {ref, reactive} from 'vue'
+import {ref} from 'vue'
 import emblaCarouselVue from 'embla-carousel-vue'
 import Autoplay from 'embla-carousel-autoplay'
-// import {GridLayout, GridItem} from 'grid-layout-plus'
-
-// const layout = reactive([
-//   {x: 0, y: 0, w: 2, h: 2, i: '0', static: false},
-//   {x: 2, y: 0, w: 2, h: 4, i: '1', static: true},
-//   {x: 4, y: 0, w: 2, h: 5, i: '2', static: false},
-//   {x: 6, y: 0, w: 2, h: 3, i: '3', static: false},
-//   {x: 8, y: 0, w: 2, h: 3, i: '4', static: false},
-//   {x: 10, y: 0, w: 2, h: 3, i: '5', static: false},
-//   {x: 0, y: 5, w: 2, h: 5, i: '6', static: false},
-//   {x: 2, y: 5, w: 2, h: 5, i: '7', static: false},
-//   {x: 4, y: 5, w: 2, h: 5, i: '8', static: false},
-//   {x: 6, y: 3, w: 2, h: 4, i: '9', static: true},
-//   {x: 8, y: 4, w: 2, h: 4, i: '10', static: false},
-//   {x: 10, y: 4, w: 2, h: 4, i: '11', static: false},
-//   {x: 0, y: 10, w: 2, h: 5, i: '12', static: false},
-//   {x: 2, y: 10, w: 2, h: 5, i: '13', static: false},
-//   {x: 4, y: 8, w: 2, h: 4, i: '14', static: false},
-//   {x: 6, y: 8, w: 2, h: 4, i: '15', static: false},
-//   {x: 8, y: 10, w: 2, h: 5, i: '16', static: false},
-//   {x: 10, y: 4, w: 2, h: 2, i: '17', static: false},
-//   {x: 0, y: 9, w: 2, h: 3, i: '18', static: false},
-//   {x: 2, y: 6, w: 2, h: 2, i: '19', static: false},
-// ])
 
 const [emblaRef] = emblaCarouselVue({loop: true, watchDrag: false}, [
   Autoplay({delay: 4000, stopOnInteraction: false, stopOnFocusIn: false}),
@@ -182,16 +158,6 @@ const featuredCollectionsLayout = [
             <template #icon> <Arrow class="w-[50px]" :font-controlled="false" /></template>
           </BaseLink>
         </div>
-        <!-- <GridLayout
-          v-model:layout="layout"
-          :row-height="30"
-          :is-draggable="false"
-          :is-resizable="false"
-        >
-          <template #item="{item}">
-            <span class="text">{{ `${item.i}${item.static ? '- Static' : ''}` }}</span>
-          </template>
-        </GridLayout> -->
       </div>
     </section>
 
@@ -457,32 +423,3 @@ const featuredCollectionsLayout = [
   opacity: 0;
 }
 </style>
-
-<!-- <style scoped>
-.vgl-layout {
-  background-color: #eee;
-}
-
-:deep(.vgl-item:not(.vgl-item--placeholder)) {
-  background-color: #ccc;
-  border: 1px solid black;
-}
-
-:deep(.vgl-item--resizing) {
-  opacity: 90%;
-}
-
-:deep(.vgl-item--static) {
-  background-color: #cce;
-}
-
-.text {
-  position: absolute;
-  inset: 0;
-  width: 100%;
-  height: 100%;
-  margin: auto;
-  font-size: 24px;
-  text-align: center;
-}
-</style> -->
