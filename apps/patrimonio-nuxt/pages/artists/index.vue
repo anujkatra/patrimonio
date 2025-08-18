3a0a00e932ce80cb1d458ac43d2166d6ad99e2ed
Anuj Katra <anujkatra94@gmail.com>
2025-08-18 18:04:49 +0530

    updated individual artist page url, added links to individual artist page from all artists page

--- apps/patrimonio-nuxt/pages/artists/index.vue
+++ apps/patrimonio-nuxt/pages/artists/index.vue
@@ -30,36 +30,40 @@ useSiteMetadata({
           v-if="artistsPageData?.featuredArtists"
           class="grid w-full grid-cols-1 gap-5 sm:grid-cols-2 lg:gap-x-[100px] lg:gap-y-10"
         >
-          <div
+          <NuxtLink
             v-for="item in artistsPageData?.featuredArtists"
             :key="item.slug.current"
-            class="group flex w-full max-w-[350px] flex-col gap-4 md:max-w-[462px] lg:max-w-[600px]"
+            :href="`/artists/${item.slug.current}`"
           >
             <div
-              class="w-full max-w-[350px] gap-4 overflow-hidden md:max-w-[462px] lg:max-w-[600px]"
+              class="group flex w-full max-w-[350px] flex-col gap-4 md:max-w-[462px] lg:max-w-[600px]"
             >
-              <NuxtImg
-                provider="sanity"
-                :src="`${item?.picture.asset?._ref}`"
-                :alt="`${item?.picture.alt}`"
-                class="aspect-[12/9] w-full object-cover object-center transition duration-300 ease-out lg:scale-[1.2] lg:group-hover:scale-100"
-              />
-            </div>
-            <div class="flex justify-between">
-              <div class="flex flex-col gap-[5px]">
-                <p class="font-satoshi text-lg/none font-medium tracking-normal">
-                  {{ item.name }}
-                </p>
-                <p class="font-satoshi text-base/none font-light tracking-normal">
-                  {{ item.location }}
-                </p>
+              <div
+                class="w-full max-w-[350px] gap-4 overflow-hidden md:max-w-[462px] lg:max-w-[600px]"
+              >
+                <NuxtImg
+                  provider="sanity"
+                  :src="`${item?.picture.asset?._ref}`"
+                  :alt="`${item?.picture.alt}`"
+                  class="aspect-[12/9] w-full object-cover object-center transition duration-300 ease-out lg:scale-[1.2] lg:group-hover:scale-100"
+                />
               </div>
-              <Arrow
-                class="w-[70px] px-2.5 transition-all duration-300 ease-out lg:pr-[30px] lg:opacity-0 lg:group-hover:pr-2.5 lg:group-hover:opacity-100"
-                :font-controlled="false"
-              />
+              <div class="flex justify-between">
+                <div class="flex flex-col gap-[5px]">
+                  <p class="font-satoshi text-lg/none font-medium tracking-normal">
+                    {{ item.name }}
+                  </p>
+                  <p class="font-satoshi text-base/none font-light tracking-normal">
+                    {{ item.location }}
+                  </p>
+                </div>
+                <Arrow
+                  class="w-[70px] px-2.5 transition-all duration-300 ease-out lg:pr-[30px] lg:opacity-0 lg:group-hover:pr-2.5 lg:group-hover:opacity-100"
+                  :font-controlled="false"
+                />
+              </div>
             </div>
-          </div>
+          </NuxtLink>
         </div>
         <!-- <NuxtImg
           v-for="(item, index) in artistsPageData?.featuredArtists"
