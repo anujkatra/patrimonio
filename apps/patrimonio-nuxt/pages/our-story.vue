648f53bcf167eb6d5f99d447af588cc965b78ab6
Anuj Katra <anujkatra94@gmail.com>
2025-08-27 01:36:53 +0530

    added team member section

--- apps/patrimonio-nuxt/pages/our-story.vue
+++ apps/patrimonio-nuxt/pages/our-story.vue
@@ -9,7 +9,7 @@ useSiteMetadata({
   description: ourStoryPageData?.value?.seo?.description ?? 'description',
   ogImage: '',
 })
-console.log('test', ourStoryPageData.value)
+console.log('test', ourStoryPageData.value?.ourTeam)
 </script>
 
 <template>
@@ -36,6 +36,45 @@ console.log('test', ourStoryPageData.value)
             <NuxtImg src="/logo.png" class="max-h-[65px] w-full" />
           </div>
         </div>
+      </div>
+    </section>
+
+    <section class="flex justify-center px-5 py-[50px] md:px-10 md:py-[70px] xl:px-[70px]">
+      <div class="flex w-full max-w-[1300px] flex-col items-center gap-[50px]">
+        <template v-if="ourStoryPageData?.ourTeam">
+          <div
+            v-for="(teamMember, index) in ourStoryPageData.ourTeam"
+            :key="index"
+            class="flex w-full flex-col gap-5"
+          >
+            <div class="w-full border-b-[0.5px] border-solid border-[#202020] pb-2.5">
+              <h1 class="font-cabinet text-[32px]/none font-normal tracking-normal">
+                {{ teamMember.title }}
+              </h1>
+            </div>
+            <div class="flex flex-col gap-5 md:flex-row">
+              <div class="order-1 w-full flex-1 md:order-2">
+                <NuxtImg
+                  provider="sanity"
+                  :src="`${teamMember?.picture.asset?._ref}`"
+                  :alt="`${teamMember?.picture.alt}`"
+                  class="aspect-[12/9] w-full object-cover object-center"
+                />
+              </div>
+              <div class="order-2 flex flex-1 flex-col gap-5 md:order-1">
+                <div>
+                  <h1 class="font-cabinet text-2xl/none tracking-normal">{{ teamMember.name }}</h1>
+                  <h1 class="font-satoshi text-lg/none tracking-normal">
+                    {{ teamMember.designation }}
+                  </h1>
+                </div>
+                <div class="flex flex-col gap-5 lg:gap-10" :v-if="teamMember.content">
+                  <SanityContent :blocks="teamMember.content ?? []" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </template>
       </div>
     </section>
   </main>
