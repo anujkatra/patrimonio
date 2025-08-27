d1a985905e8a4ad235bf16044ad992fe585bfd92
Anuj Katra <anujkatra94@gmail.com>
2025-08-27 23:37:57 +0530

    added styling for team members section

--- apps/patrimonio-nuxt/pages/our-story.vue
+++ apps/patrimonio-nuxt/pages/our-story.vue
@@ -15,17 +15,19 @@ console.log('test', ourStoryPageData.value?.ourTeam)
 <template>
   <main>
     <section class="flex justify-center px-5 py-[50px] md:px-10 md:py-[70px] xl:px-[70px]">
-      <div class="flex w-full max-w-[1300px] flex-col items-center gap-5 lg:gap-[30px] xl:gap-10">
-        <div class="flex w-full border-b-[0.5px] border-solid border-[#202020] pb-2.5">
+      <div class="flex w-full max-w-[1300px] flex-col items-center gap-5 lg:gap-[70px] xl:gap-10">
+        <div class="flex w-full border-b-[0.5px] border-solid border-[#202020] pb-5 lg:pb-8">
           <h1
-            class="font-cabinet py-1 text-[35px]/none font-normal tracking-normal lg:text-[76px] xl:text-8xl/none"
+            class="font-cabinet text-[35px]/none font-normal tracking-normal lg:text-[65px]/none lg:text-[45px] xl:text-[65px]/none"
           >
             Our Story
           </h1>
         </div>
-        <div class="flex w-full flex-col gap-5 md:flex-row lg:gap-10">
-          <div class="flex flex-1 flex-col gap-[5px]">
-            <h1 class="font-cabinet text-[32px]/none font-normal tracking-normal">
+        <div class="flex w-full flex-col gap-5 md:flex-row lg:gap-[100px] xl:gap-[155px]">
+          <div class="flex flex-1 flex-col gap-2.5">
+            <h1
+              class="font-cabinet text-[32px]/none font-normal tracking-normal md:text-[35px]/none"
+            >
               {{ ourStoryPageData?.title }}
             </h1>
             <div class="flex flex-col gap-5 lg:gap-10" :v-if="ourStoryPageData?.content">
@@ -33,27 +35,44 @@ console.log('test', ourStoryPageData.value?.ourTeam)
             </div>
           </div>
           <div class="w-full flex-1">
-            <NuxtImg src="/logo.png" class="max-h-[65px] w-full" />
+            <NuxtImg
+              src="/logo.png"
+              class="max-h-[65px] w-full md:max-h-[71px] md:max-w-[382px] xl:max-h-[79px] xl:max-w-[425px]"
+            />
           </div>
         </div>
       </div>
     </section>
 
-    <section class="flex justify-center px-5 py-[50px] md:px-10 md:py-[70px] xl:px-[70px]">
+    <section class="relative flex justify-center px-5 pb-[50px] md:px-10 md:pb-[70px] xl:px-[70px]">
+      <NuxtImg
+        src="/our-story-team-bg-mobile.png"
+        class="pointer-events-none absolute bottom-0 size-full opacity-50 md:hidden"
+      />
+      <NuxtImg
+        src="/our-story-team-bg-tablet.png"
+        class="pointer-events-none absolute bottom-0 hidden size-full md:block xl:hidden"
+      />
+      <NuxtImg
+        src="/our-story-team-bg-desktop.png"
+        class="pointer-events-none absolute bottom-0 hidden size-full xl:block"
+      />
       <div class="flex w-full max-w-[1300px] flex-col items-center gap-[50px]">
         <template v-if="ourStoryPageData?.ourTeam">
           <div
             v-for="(teamMember, index) in ourStoryPageData.ourTeam"
             :key="index"
-            class="flex w-full flex-col gap-5"
+            class="flex w-full flex-col gap-[50px] md:gap-[70px]"
           >
-            <div class="w-full border-b-[0.5px] border-solid border-[#202020] pb-2.5">
-              <h1 class="font-cabinet text-[32px]/none font-normal tracking-normal">
+            <div class="w-full border-b-[0.5px] border-solid border-[#202020] pb-5">
+              <h1
+                class="font-cabinet text-[32px]/none font-normal tracking-normal lg:text-[35px]/none"
+              >
                 {{ teamMember.title }}
               </h1>
             </div>
-            <div class="flex flex-col gap-5 md:flex-row">
-              <div class="order-1 w-full flex-1 md:order-2">
+            <div class="flex flex-col gap-5 md:flex-row lg:gap-10">
+              <div class="w-full flex-1">
                 <NuxtImg
                   provider="sanity"
                   :src="`${teamMember?.picture.asset?._ref}`"
@@ -61,10 +80,12 @@ console.log('test', ourStoryPageData.value?.ourTeam)
                   class="aspect-[12/9] w-full object-cover object-center"
                 />
               </div>
-              <div class="order-2 flex flex-1 flex-col gap-5 md:order-1">
+              <div class="flex flex-1 flex-col gap-5">
                 <div>
-                  <h1 class="font-cabinet text-2xl/none tracking-normal">{{ teamMember.name }}</h1>
-                  <h1 class="font-satoshi text-lg/none tracking-normal">
+                  <h1 class="font-cabinet text-2xl/none tracking-normal xl:text-[65px]/none">
+                    {{ teamMember.name }}
+                  </h1>
+                  <h1 class="font-satoshi text-lg/none tracking-normal lg:text-xl/none">
                     {{ teamMember.designation }}
                   </h1>
                 </div>
