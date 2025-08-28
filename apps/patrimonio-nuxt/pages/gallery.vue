a754a78fb016efcdbb2607e02e9435c16d9e7e71
Anuj Katra <anujkatra94@gmail.com>
2025-08-28 16:15:38 +0530

    add initial page content

--- apps/patrimonio-nuxt/pages/gallery.vue
+++ apps/patrimonio-nuxt/pages/gallery.vue
@@ -1,3 +1,30 @@
+<script lang="ts" setup>
+import {galleryPageQuery} from '~/sanity/queries'
+import type {GalleryPageQueryResult} from '~/sanity/types'
+
+const {data: galleryPageData} = await useSanityQuery<GalleryPageQueryResult>(galleryPageQuery)
+
+useSiteMetadata({
+  title: galleryPageData?.value?.seo?.title ?? 'title',
+  description: galleryPageData?.value?.seo?.description ?? 'description',
+  ogImage: '',
+})
+console.log('test', galleryPageData.value)
+</script>
+
 <template>
-  <p class="text-center">Gallery Page</p>
+  <main>
+    <section class="flex justify-center px-5 py-[50px] md:px-10 md:py-[70px] xl:px-[70px]">
+      <div class="flex w-full max-w-[1300px] flex-col gap-5 lg:gap-[70px] xl:gap-10">
+        <h1
+          class="font-cabinet text-[35px]/none font-normal tracking-normal lg:text-[45px]/none xl:text-[65px]/none"
+        >
+          {{ galleryPageData?.title ?? 'Gallery' }}
+        </h1>
+        <h1 class="font-satoshi text-lg/none tracking-normal lg:text-xl/none">
+          {{ galleryPageData?.subtitle ?? '' }}
+        </h1>
+      </div>
+    </section>
+  </main>
 </template>
