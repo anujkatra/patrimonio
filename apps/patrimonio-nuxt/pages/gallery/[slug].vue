eeced585e27ca645142e689fe55d9a08a6b1eb32
Anuj Katra <anujkatra94@gmail.com>
2025-09-11 00:19:12 +0530

    add individual painting page content

--- apps/patrimonio-nuxt/pages/gallery/[slug].vue
+++ apps/patrimonio-nuxt/pages/gallery/[slug].vue
@@ -1,7 +1,6 @@
 <script lang="ts" setup>
 import {paintingQuery} from '~/sanity/queries'
 import type {PaintingQueryResult} from '~/sanity/types'
-import Arrow from '~/assets/svg/arrow.svg'
 
 const route = useRoute()
 const {data: paintingData} = await useSanityQuery<PaintingQueryResult>(paintingQuery, {
@@ -16,8 +15,6 @@ if (paintingData.value === null) {
   })
 }
 
-console.log('test', paintingData.value)
-
 useSiteMetadata({
   title: `${paintingData.value.name} - ${paintingData.value.artist}`,
   description: paintingData.value.description ?? '',
@@ -27,78 +24,48 @@ useSiteMetadata({
 
 <template>
   <main>
-    <!-- <section class="flex justify-center px-5 py-[50px] md:px-10 md:py-[70px] xl:px-[70px]">
-      <div class="flex w-full max-w-[1300px] flex-col items-center gap-5 lg:gap-[30px] xl:gap-10">
-        <div class="flex w-full border-b-[0.5px] border-solid border-[#202020] pb-2.5">
-          <h1
-            class="font-cabinet py-1 text-[35px]/none font-normal tracking-normal lg:text-[76px] xl:text-8xl/none"
+    <section class="flex justify-center px-5 py-[50px] md:px-10 md:py-[70px] xl:px-[70px]">
+      <div class="flex w-full max-w-[1300px] flex-col gap-10 lg:flex-row">
+        <div class="hidden lg:flex">
+          <NuxtImg
+            provider="sanity"
+            class="aspect-square w-[600px]"
+            :src="`${paintingData?.picture?.asset?._ref}`"
+            :alt="`${paintingData?.picture?.alt}`"
+          />
+        </div>
+        <div class="flex w-full flex-col gap-2.5 lg:max-w-[540px]">
+          <p
+            class="font-cabinet text-[35px]/none font-normal tracking-normal lg:pt-5 lg:text-[45px]/none xl:text-[65px]/none"
           >
             {{ paintingData?.name }}
-          </h1>
-        </div>
-        <div class="flex w-full flex-col gap-5 md:flex-row lg:gap-10">
-          <div class="w-full flex-1">
-            <NuxtImg
-              provider="sanity"
-              :src="`${paintingData?.picture.asset?._ref}`"
-              :alt="`${paintingData?.picture.alt}`"
-              class="aspect-[12/9] w-full object-cover object-center"
-            />
-          </div>
-          <div class="flex flex-1 flex-col gap-5 lg:gap-10" :v-if="paintingData?.description">
-            <SanityContent :blocks="paintingData?.description ?? []" />
-          </div>
+          </p>
+          <NuxtImg
+            provider="sanity"
+            class="w-full lg:hidden"
+            :src="`${paintingData?.picture?.asset?._ref}`"
+            :alt="`${paintingData?.picture?.alt}`"
+          />
+          <p
+            v-if="paintingData?.description"
+            class="font-satoshi text-base/none font-light tracking-normal md:text-xl/none"
+          >
+            {{ paintingData.description }}
+          </p>
+          <p
+            class="font-satoshi border-y-[0.5px] border-solid border-[#202020] py-2.5 text-base/none tracking-normal md:text-xl/none"
+          >
+            by {{ paintingData?.artist }}
+          </p>
+          <p
+            v-if="paintingData?.year || paintingData?.medium"
+            class="font-satoshi border-b-[0.5px] border-solid border-[#202020] pb-2.5 text-base/none tracking-normal md:text-xl/none"
+          >
+            <span v-if="paintingData?.year">{{ paintingData?.year }} | </span>
+            <span v-if="paintingData?.medium">{{ paintingData?.medium }}</span>
+          </p>
         </div>
       </div>
     </section>
-
-    <section
-      v-if="paintingData?.featuredPaintings"
-      class="flex w-full flex-col items-center justify-center gap-5 overflow-hidden border-t-[0.5px] border-[#202020] py-[50px] pl-5 md:py-[70px] md:pl-10 xl:pl-[70px]"
-    >
-      <div class="mx-auto flex w-full max-w-[1370px] flex-col gap-2.5 pr-5 md:pr-10 xl:pr-[70px]">
-        <h2
-          class="font-cabinet text-[32px] leading-none font-normal tracking-normal lg:flex-1 lg:text-[50px]"
-        >
-          His Work
-        </h2>
-        <p class="font-satoshi text-lg leading-none font-light tracking-normal">
-          Discover an exquisite global collection, thoughtfully presented through curated events,
-          exhibitions, and exclusive showcases.
-        </p>
-      </div>
-      <div class="mx-auto flex w-full max-w-[1370px] flex-col justify-between gap-5">
-        <div
-          class="flex h-full w-full gap-2.5 overflow-x-auto overflow-y-hidden pr-5 [scrollbar-width:thin] md:pr-10 xl:pr-[70px]"
-        >
-          <template v-for="(painting, index) in paintingData?.featuredPaintings" :key="index">
-            <PaintingTile
-              v-if="painting?.picture?.asset != null"
-              :name="painting.name"
-              :artist="painting.artist"
-              :year="painting.year"
-              :medium="painting.medium ?? ``"
-              :image-src="painting.picture.asset"
-            />
-          </template>
-          <template v-for="(painting, index) in paintingData?.featuredPaintings" :key="index">
-            <PaintingTile
-              v-if="painting?.picture?.asset != null"
-              :name="painting.name"
-              :artist="painting.artist"
-              :year="painting.year"
-              :medium="painting.medium ?? ``"
-              :image-src="painting.picture.asset"
-            />
-          </template>
-        </div>
-        <div class="flex w-full pr-5 md:justify-end md:pr-10 xl:pr-[70px]">
-          <BaseLink variant="secondary" class="w-full" icon to="/gallery">
-            View All
-            <template #icon> <Arrow class="w-[50px]" :font-controlled="false" /></template>
-          </BaseLink>
-        </div>
-      </div>
-    </section> -->
   </main>
 </template>
