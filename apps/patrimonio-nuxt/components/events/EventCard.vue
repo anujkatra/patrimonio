01e9e1e0179d3c0188c35b19443dacfc7ad113bd
Anuj Katra <anujkatra94@gmail.com>
2025-09-12 23:22:39 +0530

    code cleanup

--- apps/patrimonio-nuxt/components/events/EventCard.vue
+++ apps/patrimonio-nuxt/components/events/EventCard.vue
@@ -1,31 +1,43 @@
 <script setup lang="ts">
 import {twMerge} from 'tailwind-merge'
-import { NuxtImg } from '#components';
-import type { SanityImageAssetDocument } from '@sanity/client';
 
 const props = defineProps<{
-  excerpt: string
-  title: string
-  dateRange:{endDate:string , startDate:string}
-  imageSrc: SanityImageAssetDocument | undefined
+  excerpt?: string
+  title?: string
+  dateRange?: {endDate: string; startDate: string}
+  imageSrc?: string
   class?: string
-  link:string
-  venue:string
+  link?: string
+  venue?: string
 }>()
 </script>
 
 <template>
-    <a :href="link" target="_blank" :class="twMerge(`first:border-y h-[400px] not-first:border-b border-patrimonio-black max-md:gap-5 flex flex-col md:flex-row md:py-7 md:justify-between lg:pl-12 items-stretch`,props.class)">
-        <div class="flex flex-col py-5 h-full gap-2.5 md:max-w-2/5 md:flex-1">
-            <label class="">UPCOMING</label>
-            <h3 class="font-cabinet text-2xl/none font-medium">{{ props.title }}</h3>
-            <p class="font-cabinet text-lg">{{ props.excerpt }}</p>
-            <p class="mt-auto">{{ props?.venue ?? "" }} {{ props?.dateRange?.startDate ? `| ${new Date(props?.dateRange?.startDate)}` : "" }}{{ props?.dateRange?.endDate ? ` -  ${new Date(props.dateRange.endDate)}` : ""}}</p>
-        </div>
-        <NuxtImg 
-        provider="sanity"
-        class="md:flex-[0.2]"
-        :src="`${props?.imageSrc?._ref}`"
-        :alt="`${props.title}`" />
-    </a>
+  <a
+    :href="link"
+    target="_blank"
+    :class="
+      twMerge(
+        `border-patrimonio-black flex h-[400px] flex-col items-stretch not-first:border-b first:border-y max-md:gap-5 md:flex-row md:justify-between md:py-7 lg:pl-12`,
+        props.class,
+      )
+    "
+  >
+    <div class="flex h-full flex-col gap-2.5 py-5 md:max-w-2/5 md:flex-1">
+      <label class="">UPCOMING</label>
+      <h3 class="font-cabinet text-2xl/none font-medium">{{ props.title }}</h3>
+      <p class="font-cabinet text-lg">{{ props.excerpt }}</p>
+      <p class="mt-auto">
+        {{ props?.venue ?? '' }}
+        {{ props?.dateRange?.startDate ? `| ${new Date(props?.dateRange?.startDate)}` : ''
+        }}{{ props?.dateRange?.endDate ? ` -  ${new Date(props.dateRange.endDate)}` : '' }}
+      </p>
+    </div>
+    <NuxtImg
+      provider="sanity"
+      class="md:flex-[0.2]"
+      :src="`${props?.imageSrc}`"
+      :alt="`${props.title}`"
+    />
+  </a>
 </template>
--- apps/patrimonio-nuxt/pages/events.vue
+++ apps/patrimonio-nuxt/pages/events.vue
@@ -1,15 +1,16 @@
 <script lang="ts" setup>
 import {eventsPageQuery} from '~/sanity/queries'
-import type { EventsPageQueryResult} from '~/sanity/types'
-import Arrow from '~/assets/svg/arrow.svg'
-import EventCard from '~/components/events/EventCard.vue';
+import type {EventsPageQueryResult} from '~/sanity/types'
+import EventCard from '~/components/events/EventCard.vue'
 
-const {data: eventsPageData} = await useSanityQuery<EventsPageQueryResult>(eventsPageQuery);
-console.log({eventsPageData})
+const {data: eventsPageData} = await useSanityQuery<EventsPageQueryResult>(eventsPageQuery)
+console.log(eventsPageData.value)
 
 useSiteMetadata({
   title: eventsPageData?.value?.seo?.title ?? 'Events',
-  description: eventsPageData?.value?.seo?.description ?? 'Keep yourself upto date with events conducted by Patrimonio Gallery',
+  description:
+    eventsPageData?.value?.seo?.description ??
+    'Keep yourself upto date with events conducted by Patrimonio Gallery',
   ogImage: '',
 })
 </script>
@@ -27,17 +28,32 @@ useSiteMetadata({
           <p class="font-satoshi text-lg leading-none font-light tracking-normal">
             {{ eventsPageData?.subtitle ?? '' }}
           </p>
-          <h2 class="text-2xl my-10">Auctions</h2>
-          <div v-for="auction in eventsPageData?.auctions">
-            <EventCard :key="auction?._id ?? auction.title" :title="auction?.title" :imageSrc="auction?.pictures?.[0]?.asset" :excerpt="auction?.excerpt" :venue="auction.venue"  />
+          <h2 class="my-10 text-2xl">Auctions</h2>
+          <div v-for="auction in eventsPageData?.auctions" :key="auction._id">
+            <EventCard
+              :title="auction?.title"
+              :image-src="auction?.pictures?.[0]?.asset?._ref"
+              :excerpt="auction?.excerpt"
+              :venue="auction.venue"
+            />
           </div>
-          <h2 class="text-2xl my-10">Art Shows</h2>
-          <div v-for="auction in eventsPageData?.artShows">
-            <EventCard :key="auction?._id ?? auction.title" :title="auction?.title" :imageSrc="auction?.pictures?.[0]?.asset" :excerpt="auction?.excerpt" :venue="auction.venue"  />
+          <h2 class="my-10 text-2xl">Art Shows</h2>
+          <div v-for="artShow in eventsPageData?.artShows" :key="artShow._id">
+            <EventCard
+              :title="artShow?.title"
+              :image-src="artShow?.pictures?.[0]?.asset?._ref"
+              :excerpt="artShow?.excerpt"
+              :venue="artShow.venue"
+            />
           </div>
-          <h2 class="text-2xl my-10">Solo Shows</h2>
-          <div v-for="auction in eventsPageData?.soloShows">
-            <EventCard :key="auction?._id ?? auction.title" :title="auction?.title" :imageSrc="auction?.pictures?.[0]?.asset" :excerpt="auction?.excerpt" :venue="auction.venue"  />
+          <h2 class="my-10 text-2xl">Solo Shows</h2>
+          <div v-for="soloShow in eventsPageData?.soloShows" :key="soloShow._id">
+            <EventCard
+              :title="soloShow?.title"
+              :image-src="soloShow?.pictures?.[0]?.asset?._ref"
+              :excerpt="soloShow?.excerpt"
+              :venue="soloShow.venue"
+            />
           </div>
         </div>
       </div>
