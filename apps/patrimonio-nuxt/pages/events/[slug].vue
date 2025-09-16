d4d3aab937c94ec753b61431d7311a7dceff1473
Anuj Katra <anujkatra94@gmail.com>
2025-09-16 14:16:26 +0530

    add landing section tabled and desktop styles

--- apps/patrimonio-nuxt/pages/events/[slug].vue
+++ apps/patrimonio-nuxt/pages/events/[slug].vue
@@ -28,43 +28,59 @@ useSiteMetadata({
     <section
       class="border-patrimonio-black flex justify-center border-b-[0.5px] px-5 py-[50px] md:px-10 md:py-[70px] xl:px-[70px]"
     >
-      <div class="flex w-full max-w-[1300px] flex-col gap-2.5">
-        <h1
-          class="font-cabinet text-[35px]/none font-normal tracking-normal lg:text-[45px]/none xl:text-[65px]/none"
-        >
-          {{ eventData?.title ?? 'Event' }}
-        </h1>
-        <NuxtImg
-          provider="sanity"
-          class="w-full"
-          :src="eventData?.pictures?.[0]?.asset?._ref"
-          :alt="eventData?.pictures?.[0].alt"
-        />
-        <p v-if="eventData?.type === 'art-show'" class="font-satoshi text-xl/none tracking-normal">
-          Art Show
-        </p>
-        <p
-          v-else-if="eventData?.type === 'auction'"
-          class="font-satoshi text-xl/none tracking-normal"
-        >
-          Auction
-        </p>
-        <p
-          v-else-if="eventData?.type === 'solo-show'"
-          class="font-satoshi text-xl/none tracking-normal"
-        >
-          Solo Show
-        </p>
-        <div class="flex flex-1 flex-col gap-5 lg:gap-10" :v-if="eventData?.content">
-          <SanityContent :blocks="eventData?.content" />
+      <div class="flex w-full max-w-[1300px] gap-5 xl:gap-10">
+        <div class="flex w-full flex-col gap-2.5 md:flex-1 md:gap-5">
+          <h1
+            class="font-cabinet text-[35px]/none font-normal tracking-normal lg:text-[45px]/none xl:text-[65px]/none"
+          >
+            {{ eventData?.title ?? 'Event' }}
+          </h1>
+          <NuxtImg
+            provider="sanity"
+            class="w-full md:hidden"
+            :src="eventData?.pictures?.[0]?.asset?._ref"
+            :alt="eventData?.pictures?.[0].alt"
+          />
+          <p
+            v-if="eventData?.type === 'art-show'"
+            class="font-satoshi text-xl/none tracking-normal"
+          >
+            Art Show
+          </p>
+          <p
+            v-else-if="eventData?.type === 'auction'"
+            class="font-satoshi text-xl/none tracking-normal"
+          >
+            Auction
+          </p>
+          <p
+            v-else-if="eventData?.type === 'solo-show'"
+            class="font-satoshi text-xl/none tracking-normal"
+          >
+            Solo Show
+          </p>
+          <div
+            class="font-satoshi flex flex-col gap-5 text-xl/none font-light tracking-normal lg:gap-10"
+            :v-if="eventData?.content"
+          >
+            <SanityContent :blocks="eventData?.content" />
+          </div>
+          <BaseLink variant="secondary" class="w-full" icon to="/">
+            Join the Auction
+            <template #icon> <Arrow class="w-[50px]" :font-controlled="false" /></template>
+          </BaseLink>
+          <BaseLink variant="primary" class="bg-patrimonio-beige w-full" to="/">
+            Download Catalogue (PDF)
+          </BaseLink>
         </div>
-        <BaseLink variant="secondary" class="w-full" icon to="/">
-          Test
-          <template #icon> <Arrow class="w-[50px]" :font-controlled="false" /></template>
-        </BaseLink>
-        <BaseLink variant="primary" class="bg-patrimonio-beige w-full" to="/">
-          View all upcoming events
-        </BaseLink>
+        <div class="hidden w-full md:block md:flex-1">
+          <NuxtImg
+            provider="sanity"
+            class="w-full"
+            :src="eventData?.pictures?.[0]?.asset?._ref"
+            :alt="eventData?.pictures?.[0].alt"
+          />
+        </div>
       </div>
     </section>
   </main>
