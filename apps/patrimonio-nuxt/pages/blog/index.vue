af468cd29adf1c6fcb4540ff80472858c379cd44
Anuj Katra <anujkatra94@gmail.com>
2025-09-17 12:15:43 +0530

    create blog page and update header link

---
+++ apps/patrimonio-nuxt/pages/blog/index.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup></script>
+
+<template>
+  <main>
+    <section class="flex justify-center px-5 py-[50px] md:px-10 md:py-[70px] xl:px-[70px]">
+      <div class="flex w-full max-w-[1300px] flex-col items-center gap-5">
+        <div class="flex w-full flex-col gap-2.5">
+          <h1
+            class="font-cabinet text-[35px] leading-none font-normal tracking-normal md:flex-1 lg:text-[50px]"
+          >
+            Blog page
+            <!-- {{ eventsPageData?.title }} -->
+          </h1>
+          <p class="font-satoshi text-lg leading-none font-light tracking-normal">
+            <!-- {{ eventsPageData?.subtitle ?? '' }} -->
+          </p>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
