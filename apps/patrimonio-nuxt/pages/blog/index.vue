cbb9b79a1ef478be80ff2a328c18fa58b5c23f26
Anuj Katra <anujkatra94@gmail.com>
2025-09-18 22:21:52 +0530

    add link to individual blogs on all blogs page

--- apps/patrimonio-nuxt/pages/blog/index.vue
+++ apps/patrimonio-nuxt/pages/blog/index.vue
@@ -64,46 +64,53 @@ useSiteMetadata({
           </p>
         </div>
         <div class="flex w-full flex-col gap-5">
-          <div
+          <NuxtLink
             v-for="blog in blogData"
             :key="blog.slug.current"
-            class="border-patrimonio-black flex w-full flex-col gap-5 border-t-[0.5px] pt-5 pb-[50px] last:border-b-[0.5px] md:flex-row lg:gap-[50px]"
+            class="w-full"
+            :to="`/blog/${blog.slug.current}`"
           >
-            <div class="flex w-full md:flex-1">
-              <NuxtImg
-                provider="sanity"
-                class="w-full object-cover object-center"
-                :src="blog?.featuredImage.asset?._ref"
-                :alt="blog?.featuredImage?.alt"
-              />
-            </div>
             <div
-              class="flex w-full flex-col gap-[15px] md:flex-1 md:gap-5 lg:gap-[25px] lg:pt-2.5 xl:pt-5"
+              class="border-patrimonio-black flex w-full flex-col gap-5 border-t-[0.5px] pt-5 pb-[50px] last:border-b-[0.5px] md:flex-row lg:gap-[50px]"
             >
-              <div class="flex w-full flex-col gap-2.5 lg:max-w-[457px]">
-                <p class="font-satoshi tracking-none text-[15px]/none uppercase">
-                  {{ blog.subtitle }}
-                </p>
-                <p class="font-cabinet tracking-none text-2xl/none font-medium">{{ blog.title }}</p>
-                <p class="font-satoshi tracking-none text-lg/none">{{ blog.excerpt }}</p>
+              <div class="flex w-full md:flex-1">
+                <NuxtImg
+                  provider="sanity"
+                  class="w-full object-cover object-center"
+                  :src="blog?.featuredImage.asset?._ref"
+                  :alt="blog?.featuredImage?.alt"
+                />
               </div>
               <div
-                class="border-patrimonio-black flex justify-between gap-2.5 border-t-[0.5px] pt-[15px] md:h-full md:flex-col lg:h-auto lg:max-w-[457px] lg:pt-[25px]"
+                class="flex w-full flex-col gap-[15px] md:flex-1 md:gap-5 lg:gap-[25px] lg:pt-2.5 xl:pt-5"
               >
-                <p class="font-satoshi tracking-none text-[15px]/none">
-                  {{ blog.author }} |
-                  <NuxtTime
-                    :datetime="blog.publishDate"
-                    year="numeric"
-                    month="short"
-                    day="numeric"
-                    locale="en-IN"
-                  />
-                </p>
-                <Arrow class="w-[50px] lg:hidden" :font-controlled="false" />
+                <div class="flex w-full flex-col gap-2.5 lg:max-w-[457px]">
+                  <p class="font-satoshi tracking-none text-[15px]/none uppercase">
+                    {{ blog.subtitle }}
+                  </p>
+                  <p class="font-cabinet tracking-none text-2xl/none font-medium">
+                    {{ blog.title }}
+                  </p>
+                  <p class="font-satoshi tracking-none text-lg/none">{{ blog.excerpt }}</p>
+                </div>
+                <div
+                  class="border-patrimonio-black flex justify-between gap-2.5 border-t-[0.5px] pt-[15px] md:h-full md:flex-col lg:h-auto lg:max-w-[457px] lg:pt-[25px]"
+                >
+                  <p class="font-satoshi tracking-none text-[15px]/none">
+                    {{ blog.author }} |
+                    <NuxtTime
+                      :datetime="blog.publishDate"
+                      year="numeric"
+                      month="short"
+                      day="numeric"
+                      locale="en-IN"
+                    />
+                  </p>
+                  <Arrow class="w-[50px] lg:hidden" :font-controlled="false" />
+                </div>
               </div>
             </div>
-          </div>
+          </NuxtLink>
         </div>
         <div class="w-full">
           <Pagination
